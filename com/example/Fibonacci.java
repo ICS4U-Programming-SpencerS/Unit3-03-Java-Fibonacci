a4c0ba82bd91e97ba98f012c1dbdc566ed153965
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-04-25 10:05:11 -0400

    Initial commit

---
+++ com/example/Fibonacci.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-04-25
+ */
+
+// Fibonacci class
+public final class Fibonacci {
+
+    /** Private constructor to prevent instantiation. */
+    private Fibonacci() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
