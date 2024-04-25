d161a268fa6b1a1ca4658b2d70208e941a2cc3dd
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-04-25 14:26:52 +0000

    Done and fixes

--- com/example/Fibonacci.java
+++ com/example/Fibonacci.java
@@ -1,5 +1,10 @@
 
-package com.example;
+//package com.example;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Change me.
@@ -23,6 +28,54 @@ public final class Fibonacci {
      * @param args Unused
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world");
+        try {
+            // preps file access and writing/reading
+            final File fileInput = new File("input.txt");
+            final File fileOutput = new File("output.txt");
+            final Scanner sc = new Scanner(fileInput);
+            final FileWriter fW = new FileWriter(fileOutput);
+            final PrintWriter write = new PrintWriter(fW);
+
+            // reads each line of file
+            while (sc.hasNextLine()) {
+                final String StrLine = sc.nextLine();
+                try {
+                    // Parse the input as int
+                    final int intLine = Integer.parseInt(StrLine);
+                    // if the input is a number, check if less than 0, no -1!
+                    if (intLine < 0) {
+                        System.out.println("Input can't be less than 0");
+                    } else {
+                        // Call function.
+                        final int fibNumber = Fib(intLine);
+                        write.println("Fib(" + intLine + ") is = " + fibNumber);
+                    }
+
+                } catch (NumberFormatException Error) {
+                    System.err.println("Error: " + StrLine + " is a invalid input!");
+                }
+            }
+
+            // program finished
+            System.out.println("Done");
+
+            // closes resources
+            write.close();
+            sc.close();
+
+        } catch (IOException err) {
+            // For when no input file is found.
+            System.err.println("Error: " + err.getMessage());
+        }
+    }
+    public static int Fib (int n) {
+        // 0 and 1 are base case values, must keep to run function
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // performs the calculates fibonacci 
+        else {
+            return Fib(n-1) + Fib(n-2);
+        }
     }
 }
